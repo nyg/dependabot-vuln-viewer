a897cd27e08100380bfc8fbcfde1dd38d6ec377b
nyg <nyg@users.noreply.github.com>
2021-12-27 22:33:56 +0100

    Sort vulnuerabilities by severity

--- pages/api/vulnerabilities.js
+++ pages/api/vulnerabilities.js
@@ -1,5 +1,10 @@
 import got from 'got'
 
+const severityImportance = { LOW: 1, MODERATE: 2, HIGH: 3, CRITICAL: 4 }
+
+const highestSeverityFirst = ({ vuln }, { vuln: another }) =>
+  severityImportance[another.severity] - severityImportance[vuln.severity]
+
 
 export default async (req, res) => {
 
@@ -54,6 +59,8 @@ export default async (req, res) => {
   const vulnCount = repos
     .map(repo => repo.alerts.totalCount)
     .reduce((a, b) => a + b, 0)
+
+  repos.map(repo => repo.alerts.nodes.sort(highestSeverityFirst))
 
   res.status(200).json({
     repos,
