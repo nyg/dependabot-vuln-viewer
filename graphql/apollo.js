8c74569090183428b1de74f41010a8bff86f8aed
nyg <nyg@users.noreply.github.com>
2022-01-16 21:40:25 +0100

    Move type policies to separate file

--- graphql/apollo.js
+++ graphql/apollo.js
@@ -1,69 +1,31 @@
 import { ApolloClient, InMemoryCache, ApolloLink, HttpLink, from } from '@apollo/client'
-import { vulnerableRepos, sumVulnCount, severityImportance } from '../utils/config'
+import { vulnerableRepos, sumVulnCount } from '../utils/config'
+import { Query, Repository } from './type-policies'
 
-const cacheOptions = {
-  typePolicies: {
-    Query: {
-      fields: {
-        search: {
-          keyArgs: ['query'],
-          merge(existing = { fetchedRepoCount: 0, vulnCount: 0, nodes: [] }, incoming, { variables, readField }) {
-
-            const vulnCount = repoRef =>
-              readField({ fieldName: 'vulnerabilityAlerts', args: { first: variables.vulnCount }, from: repoRef }).totalCount
-
-            const highestVulnCountFirst = (repoRef, anotherRepoRef) =>
-              vulnCount(anotherRepoRef) - vulnCount(repoRef)
-
-            return {
-              ...incoming,
-              nodes: existing.nodes.concat(incoming.nodes).sort(highestVulnCountFirst),
-              fetchedRepoCount: existing.fetchedRepoCount + incoming.fetchedRepoCount,
-              vulnCount: existing.vulnCount + incoming.vulnCount,
-            }
-          }
-        }
-      }
-    },
-    Repository: {
-      fields: {
-        vulnerabilityAlerts: {
-          keyArgs: false,
-          merge(existing = { nodes: [] }, incoming, { readField }) {
-
-            const severity = alertRef =>
-              readField('securityVulnerability', alertRef).severity
-
-            const highestSeverityFirst = (alertRef, anotherAlertRef) =>
-              severityImportance[severity(anotherAlertRef)] - severityImportance[severity(alertRef)]
-
-            return {
-              ...incoming,
-              nodes: existing.nodes.concat(incoming.nodes).sort(highestSeverityFirst)
-            }
-          }
-        }
-      }
-    }
-  }
-}
 
 const searchReposProcessingLink = new ApolloLink((operation, forward) => {
 
-  const observable = forward(operation)
-  if (operation.operationName !== 'SearchRepos') {
-    return observable
+  if (operation.operationName === 'SearchRepos') {
+    return forward(operation).map(response => {
+      if (response.data) {
+        response.data.search.fetchedRepoCount = response.data.search.repos.length
+        response.data.search.repos = response.data.search.repos.filter(vulnerableRepos)
+        response.data.search.vulnCount = response.data.search.repos.reduce(sumVulnCount, 0)
+      }
+
+      return response
+    })
   }
 
-  return observable.map(response => {
-    response.data.search.fetchedRepoCount = response.data.search.repos.length
-    response.data.search.repos = response.data.search.repos.filter(vulnerableRepos)
-    response.data.search.vulnCount = response.data.search.repos.reduce(sumVulnCount, 0)
-    return response
-  })
+  return forward(operation)
 })
 
+const cache = new InMemoryCache({
+  typePolicies: { Query, Repository }
+})
+
+
 export default new ApolloClient({
-  cache: new InMemoryCache(cacheOptions),
-  link: from([searchReposProcessingLink, new HttpLink()])
+  link: from([searchReposProcessingLink, new HttpLink()]),
+  cache
 })
---
+++ graphql/type-policies.js
@@ -0,0 +1,46 @@
+import { severityImportance } from '../utils/config'
+
+
+export const Query = {
+  fields: {
+    search: {
+      keyArgs: ['query'],
+      merge(existing = { fetchedRepoCount: 0, vulnCount: 0, nodes: [] }, incoming, { variables, readField }) {
+
+        const vulnCount = repoRef =>
+          readField({ fieldName: 'vulnerabilityAlerts', args: { first: variables.vulnCount }, from: repoRef }).totalCount
+
+        const highestVulnCountFirst = (repoRef, anotherRepoRef) =>
+          vulnCount(anotherRepoRef) - vulnCount(repoRef)
+
+        return {
+          ...incoming,
+          nodes: existing.nodes.concat(incoming.nodes).sort(highestVulnCountFirst),
+          fetchedRepoCount: existing.fetchedRepoCount + incoming.fetchedRepoCount,
+          vulnCount: existing.vulnCount + incoming.vulnCount,
+        }
+      }
+    }
+  }
+}
+
+export const Repository = {
+  fields: {
+    vulnerabilityAlerts: {
+      keyArgs: false,
+      merge(existing = { nodes: [] }, incoming, { readField }) {
+
+        const severity = alertRef =>
+          readField('securityVulnerability', alertRef).severity
+
+        const highestSeverityFirst = (alertRef, anotherAlertRef) =>
+          severityImportance[severity(anotherAlertRef)] - severityImportance[severity(alertRef)]
+
+        return {
+          ...incoming,
+          nodes: existing.nodes.concat(incoming.nodes).sort(highestSeverityFirst)
+        }
+      }
+    }
+  }
+}
