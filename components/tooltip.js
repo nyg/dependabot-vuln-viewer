3049005a9a6511278407981f48e117ae36fb34dd
nyg <nyg@users.noreply.github.com>
2022-12-05 21:49:51 +0100

    Fix tooltip width

--- components/tooltip.js
+++ components/tooltip.js
@@ -2,7 +2,7 @@ export default function Tooltip({ value, enabled, children }) {
    return enabled ? (
       <span className='has-tooltip relative cursor-pointer border-b border-dotted border-b-gray-700'>
          {children}
-         <div className='tooltip absolute hidden cursor-auto pt-3 left-1/2 -translate-x-1/2'>
+         <div className='tooltip absolute hidden cursor-auto pt-3 left-1/2 -translate-x-1/2 w-max'>
             <div className='bg-gray-800 rounded-md text-gray-50 p-3'>
                {value}
             </div>
