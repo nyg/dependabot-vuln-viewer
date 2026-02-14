0818c47d0cdab9d94aa14280e789a2f41ecd183f
nyg <nyg@users.noreply.github.com>
2026-02-14 11:01:44 +0100

    Upgrade eslint config file

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+
+const eslintConfig = defineConfig([
+   ...nextVitals,
+   {
+      rules: {
+         indent: ['error', 3, { SwitchCase: 1 }],
+         semi: ['error', 'never'],
+         'react-hooks/exhaustive-deps': 'off',
+         quotes: ['error', 'single'],
+         sortImports: ['error', { ignoreCase: true }]
+      }
+   },
+   globalIgnores([
+      '.next/**',
+      'out/**',
+      'build/**',
+      'next-env.d.ts',
+   ]),
+])
+
+export default eslintConfig
